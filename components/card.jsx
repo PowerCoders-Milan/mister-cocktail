ba53c6014b5c524417e46ef008bc3650842066b2
marco checchi <marco.checchi2@gmail.com>
2022-11-25 10:49:49 +0100

    changed link

--- components/card.jsx
+++ components/card.jsx
@@ -4,12 +4,13 @@ import styles from "../styles/Home.module.css";
 export default function Component({cocktail}) {
 
   return (
-    <Link href={`/cocktails/${cocktail.id}`}>
-      <div className={styles.card}>
+
+    <div className={styles.card}>
+      <Link href={`/cocktails/${cocktail.id}`}>
         <h3>{cocktail.title}</h3>
         <p>{cocktail.description}</p>
-      </div>
-    </Link>
+      </Link>
+    </div>
   )
 }
 
